12dab8641aa49515c32f2a887086f4ad8e9cf2ef
Hadzhy Hitinov <richersontv@gmail.com>
2025-02-23 19:09:07 +0100

    Created new class UserRepository!

---
+++ Example/UserRepository.swift
@@ -0,0 +1,15 @@
+//
+//  UserRepository.swift
+//  Example
+//
+//  Created by Nazar on 23.02.2025.
+//
+
+import Foundation
+
+class UserRepository: ViewController {
+    
+    func getUser() -> [User] {
+        helper.getPerson()
+    }
+}
