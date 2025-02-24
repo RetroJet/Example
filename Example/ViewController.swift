087fa4e8dcb902342bb9b7fe9d342a94750a09cc
Hadzhy Hitinov <richersontv@gmail.com>
2025-02-24 17:42:57 +0100

    Created textLabel!

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -9,13 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let helper = Helper()
-    let userRepository = UserRepository()
+   private let helper = Helper()
+   private let userRepository = UserRepository()
+    private let textLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(ciColor: .blue)
+        view.backgroundColor = UIColor(ciColor: .green)
         view.alpha = CGFloat(0.90)
+        
         
         let users = userRepository.getUsers()
         helper.addUsers(users)
@@ -23,8 +25,20 @@ class ViewController: UIViewController {
         for user in helper.getPerson() {
             print("Полное имя: \(user.infoUser.firstName) \(user.infoUser.secondName)")
         }
+        
+        updateLabel()
+        view.addSubview(textLabel)
     }
-    
+    private func updateLabel() {
+        let person = helper.getPerson().randomElement()
+        let firstname = person?.infoUser.firstName
+        let secondName = person?.infoUser.secondName
+        textLabel.text = "\(firstname ?? "") \(secondName ?? "")"
+        textLabel.font = .systemFont(ofSize: 25, weight: .bold)
+        textLabel.textColor = .red
+        textLabel.frame = .init(x: 100, y: 100, width: 100, height: 50)
+    }
 }
+
 
 
